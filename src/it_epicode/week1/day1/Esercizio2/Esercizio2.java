5a8aed7d51e39ea32e6bb55c258ea8a91a922a38
LorenzoN13 <lorenzoneroni13@gmail.com>
2024-01-09 09:52:57 +0100

    in corso

---
+++ src/it_epicode/week1/day1/Esercizio2/Esercizio2.java
@@ -0,0 +1,27 @@
+package it_epicode.week1.day1.Esercizio2;
+
+import java.util.Scanner;
+
+public class Esercizio2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] parole = new String[3];
+
+        for (int i = 0; i < parole.length; i++) {
+            System.out.println("Dammi la stringa");
+            parole[i] = scanner.next();
+        }
+
+        for (int i = 0; i < parole.length; i++) {
+            System.out.print(parole[i]);
+        }
+
+        System.out.println();
+
+        for (int i = parole.length - 1; i >= 0; i--) {
+            System.out.print(parole[i]);
+        }
+    }
+}
