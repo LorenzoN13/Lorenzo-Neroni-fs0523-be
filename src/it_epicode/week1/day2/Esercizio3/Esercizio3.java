8f7e6cf85b21da3a2aef7956047fd0ed9a8d620f
LorenzoN13 <lorenzoneroni13@gmail.com>
2024-01-09 17:48:57 +0100

    fine esercizi

--- src/it_epicode/week1/day2/Esercizio3/Esercizio3.java
+++ src/it_epicode/week1/day2/Esercizio3/Esercizio3.java
@@ -1,7 +1,26 @@
 package it_epicode.week1.day2.Esercizio3;
 
+import java.util.Scanner;
+
 public class Esercizio3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String p;
+
+        do{
+            System.out.println("inserisci una parola");
+            p = scanner.nextLine();
+
+            if (!p.equals(":q")){
+                for (int i = 0; i < p.length(); i++){
+                    System.out.print(p.charAt(i));
+
+                    if (i != p.length() -1)
+                        System.out.print(",");
+                }
+                System.out.println();
+            }
+        } while(!p.equals(":q"));
     }
 }
