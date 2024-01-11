b120c59b91e115205018e719a85a04fe37425246
LorenzoN13 <lorenzoneroni13@gmail.com>
2024-01-11 18:10:26 +0100

    fine

---
+++ src/it_epicode/week1/day4/Esercizio1/Dipartimento.java
@@ -0,0 +1,5 @@
+package it_epicode.week1.day4.Esercizio1;
+
+public enum Dipartimento {
+    PRODUZIONE, AMMINISTRAZIONE, VENDITE
+}
---
+++ src/it_epicode/week1/day4/Esercizio1/Livello.java
@@ -0,0 +1,5 @@
+package it_epicode.week1.day4.Esercizio1;
+
+public enum Livello {
+    IMPIEGATO, OPERAIO, QUADRO, DIRIGENTE
+}
