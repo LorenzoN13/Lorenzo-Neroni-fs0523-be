118d10c85d7984ba5093f8b22bd76da24debf3d6
LorenzoN13 <lorenzoneroni13@gmail.com>
2024-01-12 14:24:21 +0100

    in corso

--- src/it_epicode/week1/progetto/Avvia.java
+++ src/it_epicode/week1/progetto/Avvia.java
@@ -2,6 +2,4 @@ package it_epicode.week1.progetto;
 
 public interface Avvia extends Multimediale {
     public void play();
-    public  void aumentaLuminosita();
-    public  void diminuisciLuminosita();
 }
