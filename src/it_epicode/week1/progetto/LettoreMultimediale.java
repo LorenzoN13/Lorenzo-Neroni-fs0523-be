3a073369aa996395ef339fc508629908d1385cf6
LorenzoN13 <lorenzoneroni13@gmail.com>
2024-01-12 16:59:33 +0100

    in corso

--- src/it_epicode/week1/progetto/LettoreMultimediale.java
+++ src/it_epicode/week1/progetto/LettoreMultimediale.java
@@ -10,32 +10,32 @@ public class LettoreMultimediale {
         listaMultimediale[i] = media;
     }
 
-    public void avvia(){
-        while(true){
-            System.out.println("\nMenù:");
-            int i = 0;
-            while (i < listaMultimediale.length){
+    public void avvia() {
+        while (true) {
+            System.out.println("\nMenu:");
+            for (int i = 0; i < listaMultimediale.length; i++) {
                 if (listaMultimediale[i] != null) {
                     System.out.println((i + 1) + ". " + listaMultimediale[i].getClass().getSimpleName() + ": " + listaMultimediale[i].toString());
                 }
-                i++;
             }
 
             System.out.println("0. Uscita");
             System.out.print("Seleziona un media da eseguire (1-5 o 0 per uscire): ");
 
-            int opzione;
+            int scelta;
+            try {
+                scelta = Integer.parseInt(scanner.nextLine());
 
-            try{
-                opzione = Integer.parseInt(scanner.nextLine());
-
-                if (opzione == 0){
+                if (scelta == 0) {
                     break;
-                } else if (opzione >= 1 && opzione <= 5 && listaMultimediale[opzione - 1] !=null) {
-                    listaMultimediale[opzione].esegui();
-                } else System.out.println("Scelta sbaglita. Inserisci un numero tra 1 e 5.");
-            } catch (NumberFormatException e){ System.out.println("Input errato. Inserire un numero.");}
-
+                } else if (scelta >= 1 && scelta <= 5 && listaMultimediale[scelta - 1] != null) {
+                    listaMultimediale[scelta - 1].esegui();
+                } else {
+                    System.out.println("Scelta non valida. Inserisci un numero tra 1 e 5.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Input non valido. Inserisci un numero.");
+            }
         }
     }
 }
