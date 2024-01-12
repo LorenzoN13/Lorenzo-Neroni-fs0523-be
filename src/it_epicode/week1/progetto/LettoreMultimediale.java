61bbf262b8483a0b213af8dc5453beb1db3f367e
LorenzoN13 <lorenzoneroni13@gmail.com>
2024-01-12 17:01:23 +0100

    in corso

--- src/it_epicode/week1/progetto/LettoreMultimediale.java
+++ src/it_epicode/week1/progetto/LettoreMultimediale.java
@@ -22,14 +22,14 @@ public class LettoreMultimediale {
             System.out.println("0. Uscita");
             System.out.print("Seleziona un media da eseguire (1-5 o 0 per uscire): ");
 
-            int scelta;
+            int opzione;
             try {
-                scelta = Integer.parseInt(scanner.nextLine());
+                opzione = Integer.parseInt(scanner.nextLine());
 
-                if (scelta == 0) {
+                if (opzione == 0) {
                     break;
-                } else if (scelta >= 1 && scelta <= 5 && listaMultimediale[scelta - 1] != null) {
-                    listaMultimediale[scelta - 1].esegui();
+                } else if (opzione >= 1 && opzione <= 5 && listaMultimediale[opzione - 1] != null) {
+                    listaMultimediale[opzione - 1].esegui();
                 } else {
                     System.out.println("Scelta non valida. Inserisci un numero tra 1 e 5.");
                 }
